59f7833a5c9a5949490cefafaca97e9ad986828b
bankjirapan <jirapan_yankhan@hotmail.com>
2022-12-28 08:44:14 +0700

    [EDIT] PrintScreen plugin

--- geonode_mapstore_client/client/js/plugins/actionnavbar/buttons.jsx
+++ geonode_mapstore_client/client/js/plugins/actionnavbar/buttons.jsx
@@ -16,6 +16,10 @@ import {
     toggleFullscreen
 } from '@mapstore/framework/actions/fullscreen';
 
+import {
+    togglePrintScreenTool
+} from '@js/plugins/PrintScreen'
+
 import Message from '@mapstore/framework/components/I18N/Message';
 import Button from '@js/components/Button';
 import FaIcon from '@js/components/FaIcon';
@@ -173,22 +177,30 @@ export const AnnotationsActionButton = connect(
     );
 });
 
-export const PrintScreenActionButton = connect(
-    () => ({}),
-    { onClick: setControlProperty.bind(null, 'prtsc', 'enabled', true) }
+export const PrintScreenActionButton = connect(createSelector([
+    state => state?.controls?.prtsc?.enabled || false
+], (enabled) => ({
+    enabled
+})), {
+    onClick: (enabled) => togglePrintScreenTool(enabled)
+}
 )(({
     onClick,
     variant,
-    size
+    size,
+    enabled
 }) => {
+    const PrintScreenButton = tooltip(Button);
     return (
-        <Button
+        <PrintScreenButton
+            tooltipPosition={"top"}
+            tooltip={ "Print Capture screen"  }
             variant={variant}
             size={size}
-            onClick={() => onClick()}
+            onClick={() => onClick(!enabled)}
         >
-            <Message msgId="printScreenPlugin.title" />
-        </Button>
+            <FaIcon name={"camera"} />
+        </PrintScreenButton>
     );
 });
 
